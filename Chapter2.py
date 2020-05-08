c5cbb8d63db6a57f6833e6b39268ec44399b3eb8
lhuang349 <lhuang349@gatech.edu>
2020-05-07 20:43:16 -0500

    chapter3

---
+++ Chapter2.py
@@ -0,0 +1,6 @@
+symbols = '$¢£¥€¤'
+codes = [ord(symbol) for symbol in symbols]
+print(codes)
+slice(0, 6)
+l = [1, 2, 3]
+print( id(l))
---
+++ Chapter3.py
@@ -0,0 +1,2 @@
+from collections import
+d = StrKeyDict0([('2', 'two'), ('4', 'four')])
